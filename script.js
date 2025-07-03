a9530b66d67567277df15f18fce0bc48330f97ad
OstrVlad <akimshebalkov@gmail.com>
2025-07-03 19:41:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1031,7 +1031,7 @@ function showPersonalizedModal(guestName) {
     const closeBtn = modal.querySelector(".modal-close");
 
     if (guestName) {
-        modalTitle.textContent = `Дорогой(ая) ${guestName}!`;
+        modalTitle.textContent = `Дорогие ${guestName}!`;
         modalText.innerHTML = `Мы с нетерпением ждем Вас на нашей свадьбе! <br>Ваше присутствие для нас очень важно.`;
     } else {
         modalTitle.textContent = `Дорогой гость!`;
